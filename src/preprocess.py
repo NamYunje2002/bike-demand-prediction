c150dad7de74a26c6526a1f4b1281bfa4ecb0e44
namyunje <namyunje2002@gmail.com>
2025-06-05 10:25:05 +0900

    feat: 데이터 전처리 코드 수정 및 데이터셋 변경
    - 'Dew_point_temperature°C', 'Solar_Radiation_MJ_m2' 제거 컬럼에서 제외
    - 년도 컬럼 삭제
    - 'hour_sin', 'hour_cos' 컬럼 추가

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,11 +1,10 @@
 import pandas as pd
+import numpy as np
 
 def load_and_preprocess(csv_path: str) -> pd.DataFrame:
     df = pd.read_csv(csv_path, encoding='utf-8')
 
     df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
-
-    df = df[df['Functioning Day'] == 'Yes']
 
     df['Holiday'] = df['Holiday'].map({'No Holiday': 0, 'Holiday': 1})
 
@@ -13,14 +12,14 @@ def load_and_preprocess(csv_path: str) -> pd.DataFrame:
     df['Seasons'] = pd.Categorical(df['Seasons'], categories=season_order, ordered=True)
     df['Seasons'] = df['Seasons'].cat.codes
 
-    df['year'] = df['Date'].dt.year
     df['month'] = df['Date'].dt.month
     df['weekday'] = df['Date'].dt.weekday
+    df['hour_sin'] = np.sin(2 * np.pi * df['Hour'] / 24)
+    df['hour_cos'] = np.cos(2 * np.pi * df['Hour'] / 24)
 
-    drop_cols = ['Date', 'Functioning Day', 'Dew point temperature(°C)', 'Solar Radiation (MJ/m2)']
+    drop_cols = ['Date', 'Functioning Day']
 
     df.drop(columns=drop_cols, inplace=True)
 
     df.columns = [col.strip().replace(" ", "_").replace("(", "").replace(")", "").replace("/", "_") for col in df.columns]
-
     return df
